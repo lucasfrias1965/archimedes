481531a6e865f485f5cee9f8466c2cb939b22a52
iamkillroy <lucasfrias@mailfence.com>
2026-08-12 18:35:50 -0500

    Rewrote purpose and file access

--- main.c
+++ main.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char * argv[]){
 	
     /*Code by Lucas Frias — make cool stuff!!
     Copyright 2026 MIT License 2.0
-    Archimedes: “Give me a point to stand on and I will
-    rotate the Earth”
-    A simple compilier for C like byte logic
+    Archimedes: "Give me a point to stand on and I will
+    rotate the Earth"
+    A simple compilier for boolean logic to x86
     Given simple declarations, will make a precompilied program
     that ouputs the result of logic
     */
 
-    FILE * fptr;
+    FILE * fptr_read;
     char * out_file;
-
-    if (argc == 0){
-        printf(“ERR: must specify file”);
-        return 1;
-    }
-    if (access(argv[0]) != F_OK){
-        printf(“ERR: accessed forbidden”);
-        return 1;
-    }
-
-    FILE * fptr = fopen(argv[0]);
-    char * out_file = argv[1];
     
+    
+    printf("%d is res\n", argc);
+    if (argc < 2){
+        printf("ERR: must specify file");
+        return 1;
+    }
+    if (access(argv[1], R_OK)){
+        printf("ERR: cannot read file");
+        return 1;
+    }
+
+    fptr_read = fopen(argv[0], "r");
+    out_file = argv[2];
     
     
     
