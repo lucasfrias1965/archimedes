6d8f40934cb89fe0bc87c3b7b8fd4c36ffbd790e
iamkillroy <lucasfrias@mailfence.com>
2026-08-12 18:19:57 -0500

    Implemented short project goals

--- main.c
+++ main.c
@@ -3,11 +3,33 @@
 
 int main(int argc, char * argv[]){
 	
+    /*Code by Lucas Frias — make cool stuff!!
+    Copyright 2026 MIT License 2.0
+    Archimedes: “Give me a point to stand on and I will
+    rotate the Earth”
+    A simple compilier for C like byte logic
+    Given simple declarations, will make a precompilied program
+    that ouputs the result of logic
+    */
+
+    FILE * fptr;
+    char * out_file;
+
     if (argc == 0){
         printf(“ERR: must specify file”);
-        return 0;
+        return 1;
     }
-	
-	
-	return 0;
+    if (access(argv[0]) != F_OK){
+        printf(“ERR: accessed forbidden”);
+        return 1;
+    }
+
+    FILE * fptr = fopen(argv[0]);
+    char * out_file = argv[1];
+    
+    
+    
+    
+    
+    return 0;
 }
