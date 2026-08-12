5800f620428afcffc0891ee1852c8a46b8b1f3ab
iamkillroy <lucasfrias@mailfence.com>
2026-08-12 18:06:24 -0500

    More testing

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-
-int main(){
-	printf(“Hello world!”);
+int main(int argc, char * argv[]){
+	
+    if (argc == 0){
+        printf(“ERR: must specify file”);
+        return 0;
+    }
+	
+	
 	return 0;
 }
